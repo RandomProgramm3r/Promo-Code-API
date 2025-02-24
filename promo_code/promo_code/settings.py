874d072ec1c76bbb726d136f556eacdffface1fc
Max <justamailnt@gmail.com>
2025-02-24 22:11:29 +0300

    refactor: Connect Postgresql.

--- promo_code/promo_code/settings.py
+++ promo_code/promo_code/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'promo_code.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USERNAME'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     },
 }
 
